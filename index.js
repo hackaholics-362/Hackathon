637cf1b93b10fc272b1db4a281c6a29e1b97fa5d
Connor Smith <BHa0151705@devcorptenant.com>
2026-01-28 15:33:08 +0000

    PowerBI Integration

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const a= "hiiiii i m here"
+(async () => {
+  const models = window['powerbi-client'].models;
+
+  // Call your Function to get embed configuration
+  const embedCfg = await fetch("/api/get-embed-config").then(r => r.json());
+
+  const config = {
+    type: 'report',
+    id: embedCfg.id,
+    embedUrl: embedCfg.embedUrl,
+    tokenType: models.TokenType.Embed,
+    accessToken: embedCfg.accessToken,  // short-lived
+    permissions: models.Permissions.All,
+    settings: { filterPaneEnabled: true, navContentPaneEnabled: true }
+  };
+
+  const report = powerbi.embed(document.getElementById('reportContainer'), config);
+
+  // ... same filter/search code as above ...
+})();
